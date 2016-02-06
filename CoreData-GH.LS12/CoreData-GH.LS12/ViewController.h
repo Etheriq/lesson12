23c53bdfbc86ba8f737cf7f079767a940f4323d7
Yuriy Tarnavskiy <yuriy.tarnavskiy@ekreative.com>
2016-02-06 12:36:12 +0200

    init

---
+++ CoreData-GH.LS12/CoreData-GH.LS12/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreData-GH.LS12
+//
+//  Created by Yuriy T on 06.02.16.
+//  Copyright © 2016 Yuriy T. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CoreData-GH.LS12/CoreData-GH.LS12/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CoreData-GH.LS12
+//
+//  Created by Yuriy T on 06.02.16.
+//  Copyright © 2016 Yuriy T. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
